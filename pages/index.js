1a9f4dfd52f5b92ae7511f3d3bc1cd8c522e8a82
Coner Murphy <coner@conermurphy.com>
2020-06-05 06:56:26 +0100

    changed structure of Index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,3 @@
-import Head from 'next/head';
+const Home = () => <div></div>;
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Covid 19 Analyser</title>
-      </Head>
-    </div>
-  );
-}
+export default Home;
