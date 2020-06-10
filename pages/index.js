edf9f748a0add60665dcbf9c5ce94d471f39d4bc
Coner Murphy <coner@conermurphy.com>
2020-06-10 12:46:04 +0100

    started example section

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,9 @@ const ContentSection = styled.section`
 `;
 
 const TextContent = styled.div`
-  padding-left: 10rem;
+  padding-left: ${props => (props.left ? '25%' : 0)};
+  padding-right: ${props => (props.right ? '25%' : 0)};
+  text-align: ${props => (props.right ? 'right' : 'left')};
 `;
 
 const HomeCanvas = styled.canvas`
@@ -41,7 +43,7 @@ const Home = props => (
       <HomeCanvas id="homeChart"></HomeCanvas>
     </ContentSection>
     <ContentSection coloured colNum="2">
-      <TextContent>
+      <TextContent left>
         <h2>COVID-19</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas ut
@@ -55,11 +57,25 @@ const Home = props => (
       </TextContent>
       <CovidCanvas id="covidChart"></CovidCanvas>
     </ContentSection>
-    <ContentSection>
-      <h2>About</h2>
+    <ContentSection colNum="2">
+      <CovidCanvas id="covidChart"></CovidCanvas>
+      <TextContent right>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas ut
+          laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum. Sed
+          sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi. Sed tincidunt consequat felis, nec tristique nisl
+          malesuada tincidunt. Morbi pretium turpis et dolor venenatis fermentum. In hac habitasse platea dictumst. Vestibulum posuere enim
+          elit, id consectetur sem varius eget. In nunc nibh, vehicula ut purus eu, laoreet maximus dolor. Fusce suscipit elementum eros. In
+          consequat vitae risus laoreet rhoncus. Donec placerat, nisl ut ornare vulputate, mauris felis congue turpis, in commodo metus
+          mauris ut ligula. Suspendisse vitae tortor lacus. Suspendisse at sem ac arcu tristique scelerisque id vitae lorem.{' '}
+        </p>
+      </TextContent>
     </ContentSection>
-    <ContentSection coloured>
-      <h2>Examples</h2>
+    <ContentSection coloured colNum="3">
+      <CovidCanvas id="covidChart"></CovidCanvas>
+      <CovidCanvas id="covidChart"></CovidCanvas>
+      <CovidCanvas id="covidChart"></CovidCanvas>
     </ContentSection>
   </PageContainer>
 );
