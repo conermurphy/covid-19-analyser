22a2501bc950cf379429c6eb6f871a0bf151f321
Coner Murphy <coner@conermurphy.com>
2020-06-11 20:17:56 +0100

    completed basic setup of chartjs for home chart

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import Chart from 'chart.js';
+import React, { useState, useEffect } from 'react';
 
 const PageContainer = styled.div`
   display: flex;
@@ -39,73 +40,120 @@ const TextContent = styled.div`
   max-width: 40%;
 `;
 
-const HomeCanvas = styled.canvas`
-  width: 50%;
-  max-width: 70%;
-  background-color: blue;
+const HomeChartContainer = styled.div`
+  position: relative;
   margin: auto;
+  height: 70vh;
+  width: 50%;
+  max-width: 75%;
   z-index: 2;
 `;
 
-const CovidCanvas = styled.canvas`
-  width: 400px;
-  height: 400px;
+const CovidCanvasContainer = styled.div`
+  position: relative;
+  margin: auto;
+  height: 20vw;
+  width: 10%;
   max-width: 400px;
   background-color: blue;
-  margin: auto;
 `;
 
-const Home = props => (
-  <PageContainer>
-    <ContentSection>
-      <HomeCanvas id="homeChart"></HomeCanvas>
-    </ContentSection>
-    <ContentSection id="covid" coloured beforeEl>
-      <TextContent left>
-        <h2>COVID-19</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas ut
-          laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum. Sed
-          sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi. Sed tincidunt consequat felis, nec tristique nisl
-          malesuada tincidunt. Morbi pretium turpis et dolor venenatis fermentum. In hac habitasse platea dictumst. Vestibulum posuere enim
-          elit, id consectetur sem varius eget. In nunc nibh, vehicula ut purus eu, laoreet maximus dolor. Fusce suscipit elementum eros. In
-          consequat vitae risus laoreet rhoncus. Donec placerat, nisl ut ornare vulputate, mauris felis congue turpis, in commodo metus
-          mauris ut ligula. Suspendisse vitae tortor lacus. Suspendisse at sem ac arcu tristique scelerisque id vitae lorem.{' '}
-        </p>
-      </TextContent>
-      <CovidCanvas id="covidChart"></CovidCanvas>
-    </ContentSection>
-    <ContentSection id="about">
-      <CovidCanvas id="covidChart"></CovidCanvas>
-      <TextContent right>
-        <h2>About</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas ut
-          laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum. Sed
-          sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi. Sed tincidunt consequat felis, nec tristique nisl
-          malesuada tincidunt. Morbi pretium turpis et dolor venenatis fermentum. In hac habitasse platea dictumst. Vestibulum posuere enim
-          elit, id consectetur sem varius eget. In nunc nibh, vehicula ut purus eu, laoreet maximus dolor. Fusce suscipit elementum eros. In
-          consequat vitae risus laoreet rhoncus. Donec placerat, nisl ut ornare vulputate, mauris felis congue turpis, in commodo metus
-          mauris ut ligula. Suspendisse vitae tortor lacus. Suspendisse at sem ac arcu tristique scelerisque id vitae lorem.{' '}
-        </p>
-      </TextContent>
-    </ContentSection>
-    <ContentSection id="examples" coloured column beforeEl>
-      <TextContent style={{ textAlign: 'center', width: '50%', margin: 'auto', paddingBottom: '2rem' }}>
-        <h2>Examples</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas ut
-          laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum. Sed
-          sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi.
-        </p>
-      </TextContent>
-      <div style={{ display: 'flex', flexDirection: 'row', width: '75%', margin: 'auto' }}>
-        <CovidCanvas id="covidChart"></CovidCanvas>
-        <CovidCanvas id="covidChart"></CovidCanvas>
-        <CovidCanvas id="covidChart"></CovidCanvas>
-      </div>
-    </ContentSection>
-  </PageContainer>
-);
+const Home = () => {
+  useEffect(() => {
+    const homeCtx = document.getElementById('homeChart').getContext('2d');
+    const homeChartOptions = {
+      legend: {
+        display: false,
+      },
+    };
+    const homeChartData = {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8],
+      datasets: [
+        {
+          label: 'Covid-19 Cases / Status / Country',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: '#D7D4ED',
+          pointBackgroundColor: '#D7D4ED',
+          borderWidth: 5,
+          fill: 'none',
+          lineTension: 0,
+          data: [12, 10, 30, 24, 0, 21, 12, 3],
+        },
+      ],
+    };
+    const homeChart = new Chart(homeCtx, {
+      type: 'line',
+      data: homeChartData,
+      options: homeChartOptions,
+    });
+  }, []);
+
+  return (
+    <PageContainer>
+      <ContentSection>
+        <HomeChartContainer>
+          <canvas id="homeChart"></canvas>
+        </HomeChartContainer>
+      </ContentSection>
+      <ContentSection id="covid" coloured beforeEl>
+        <TextContent left>
+          <h2>COVID-19</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas
+            ut laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum.
+            Sed sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi. Sed tincidunt consequat felis, nec tristique
+            nisl malesuada tincidunt. Morbi pretium turpis et dolor venenatis fermentum. In hac habitasse platea dictumst. Vestibulum
+            posuere enim elit, id consectetur sem varius eget. In nunc nibh, vehicula ut purus eu, laoreet maximus dolor. Fusce suscipit
+            elementum eros. In consequat vitae risus laoreet rhoncus. Donec placerat, nisl ut ornare vulputate, mauris felis congue turpis,
+            in commodo metus mauris ut ligula. Suspendisse vitae tortor lacus. Suspendisse at sem ac arcu tristique scelerisque id vitae
+            lorem.{' '}
+          </p>
+        </TextContent>
+        <CovidCanvasContainer>
+          <canvas id="covidChart"></canvas>
+        </CovidCanvasContainer>
+      </ContentSection>
+      <ContentSection id="about">
+        <CovidCanvasContainer>
+          <canvas id="covidChart"></canvas>
+        </CovidCanvasContainer>
+        <TextContent right>
+          <h2>About</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas
+            ut laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum.
+            Sed sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi. Sed tincidunt consequat felis, nec tristique
+            nisl malesuada tincidunt. Morbi pretium turpis et dolor venenatis fermentum. In hac habitasse platea dictumst. Vestibulum
+            posuere enim elit, id consectetur sem varius eget. In nunc nibh, vehicula ut purus eu, laoreet maximus dolor. Fusce suscipit
+            elementum eros. In consequat vitae risus laoreet rhoncus. Donec placerat, nisl ut ornare vulputate, mauris felis congue turpis,
+            in commodo metus mauris ut ligula. Suspendisse vitae tortor lacus. Suspendisse at sem ac arcu tristique scelerisque id vitae
+            lorem.{' '}
+          </p>
+        </TextContent>
+      </ContentSection>
+      <ContentSection id="examples" coloured column beforeEl>
+        <TextContent style={{ textAlign: 'center', width: '50%', margin: 'auto', paddingBottom: '2rem' }}>
+          <h2>Examples</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tortor orci, porta a mollis in, vestibulum at sapien. Maecenas
+            ut laoreet eros, id pharetra libero. Aenean ex justo, laoreet id fermentum vel, rhoncus placerat metus. Morbi sed blandit ipsum.
+            Sed sit amet magna nec metus feugiat molestie quis sed lectus. Nam vitae diam nisi.
+          </p>
+        </TextContent>
+        <div style={{ display: 'flex', flexDirection: 'row', width: '75%', margin: 'auto' }}>
+          <CovidCanvasContainer>
+            <canvas id="covidChart"></canvas>
+          </CovidCanvasContainer>
+          <CovidCanvasContainer>
+            <canvas id="covidChart"></canvas>
+          </CovidCanvasContainer>
+          <CovidCanvasContainer>
+            <canvas id="covidChart"></canvas>
+          </CovidCanvasContainer>
+        </div>
+      </ContentSection>
+    </PageContainer>
+  );
+};
 
 export default Home;
