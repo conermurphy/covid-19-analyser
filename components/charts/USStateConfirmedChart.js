0851ba87ffb13bb12e8f3d2de51f61a01f45306c
Coner Murphy <coner@conermurphy.com>
2020-06-22 23:10:57 +0100

    finished us state confirmed cases chart

--- components/charts/USStateConfirmedChart.js
+++ components/charts/USStateConfirmedChart.js
@@ -6,20 +6,9 @@ const USStateConfirmedChart = ({ API }) => {
   const USStateConfirmedChartRef = useRef(null);
   const [usStateList, setUsStateList] = useState();
   const [usStateConfirmedData, setUsStateConfirmedData] = useState();
+  const [usStateConfirmedLabels, setUsStateConfirmedLabels] = useState();
 
   // Function to request the data for the province state from the API.
-
-  async function getConfirmedData(PS) {
-    const data = await request(
-      API,
-      `query {
-        getTimeSeries(combinedKey:"${PS}"){
-          combinedKey
-        }
-      }`
-    );
-    return data;
-  }
 
   const getUSStates = `query {
     getProvinceState(countryRegion:"US") {
@@ -35,6 +24,7 @@ const USStateConfirmedChart = ({ API }) => {
       const usProvinceStates = usData.getProvinceState.map(ps => ps.provinceState);
       const uniqueUsData = Array.from(new Set(usProvinceStates))
         .filter(ps => ps !== '')
+        .map(ps => ps.replace(/([ ])/g, '-'))
         .sort((a, b) => (a < b ? -1 : 1));
       setUsStateList(uniqueUsData);
     };
@@ -44,11 +34,83 @@ const USStateConfirmedChart = ({ API }) => {
   // Fetch confirmed data and set to state
 
   useEffect(() => {
+    async function getConfirmedData(PS) {
+      const data = await request(
+        API,
+        `query {
+            getTimeSeries(combinedKey:"${PS}-US"){
+              confirmed
+            }
+          }`
+      );
+      return data.getTimeSeries[0].confirmed;
+    }
+
     const fetchStateConfirmedData = async () => {
-      console.log(usStateList);
+      if (typeof usStateList !== 'undefined') {
+        const parsedData = await Promise.all(
+          usStateList.map(
+            sl =>
+              new Promise(async (res, rej) => {
+                try {
+                  const data = await getConfirmedData(sl);
+                  const dataValues = Object.values(data);
+                  const latestConfirmed = dataValues[dataValues.length - 1];
+                  const psData = {};
+                  psData[sl] = latestConfirmed;
+                  res(psData);
+                } catch (err) {
+                  console.error(err);
+                  rej(err);
+                }
+              })
+          )
+        );
+
+        const labels = parsedData.map(a => Object.keys(a)).flat();
+        const data = parsedData.map(a => Object.values(a)).flat();
+
+        setUsStateConfirmedLabels(labels);
+        setUsStateConfirmedData(data);
+      }
     };
     fetchStateConfirmedData();
-  }, [usStateList]);
+  }, [API, usStateList]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (typeof window.USStateConfirmedChart !== 'undefined') {
+        window.USStateConfirmedChart.destroy();
+      }
+
+      const options = {
+        title: {
+          display: true,
+          fontSize: 20,
+          fontFamily: 'Montserrat',
+          text: 'US State Confirmed Cases / State',
+        },
+        legend: {
+          display: false,
+        },
+      };
+
+      const data = {
+        datasets: [{ data: usStateConfirmedData }],
+        labels: usStateConfirmedLabels,
+      };
+
+      window.USStateConfirmedChart = new Chart(USStateConfirmedChartRef.current, {
+        type: 'pie',
+        data,
+        options,
+      });
+
+      if (typeof window.USStateConfirmedChart !== 'undefined') {
+        window.USStateConfirmedChart.update();
+      }
+    }
+  }, [usStateConfirmedData, usStateConfirmedLabels]);
 
   return <canvas ref={USStateConfirmedChartRef}></canvas>;
 };
